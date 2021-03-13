7704df4a004378fbf2fbc33410415308a55cd7ed
yadiiiig <willemteughels@gmail.com>
2021-03-13 23:42:12 +0100

    Refactored build code

--- build.go
+++ build.go
@@ -2,25 +2,21 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 )
 
-var (
-	files []string
-)
-
 func main() {
-	getFiles("./commands")
+	files := getFiles("./commands")
 	fmt.Printf("\n")
 	for _, file := range files {
 		build(file)
 	}
 }
 
-func getFiles(dir string) {
+func getFiles(dir string) []string {
+	var files []string
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
 			files = append(files, path)
@@ -28,16 +24,20 @@ func getFiles(dir string) {
 		return nil
 	})
 	if err != nil {
-		panic(err)
+		fmt.Println("Error encountered:")
+		fmt.Println(err)
+		os.Exit(1)
 	}
+	return files
 }
 
 func build(file string) {
 	cmd := exec.Command("go", "build", "-o", "bin", file)
 	err := cmd.Run()
 	if err != nil {
-		fmt.Println("hola")
-		log.Fatal(err)
+		fmt.Println("Error encountered: \n")
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
 	fmt.Println("Sucessfully build:", file)
