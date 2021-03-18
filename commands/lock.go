1d552ee8f86eb5b98ebc3e04b9d5c42f37a2099c
yadiiiig <willemteughels@gmail.com>
2021-03-18 23:10:00 +0100

    Disabling lock command for non windows OS'ses

--- commands/lock.go
+++ commands/lock.go
@@ -4,9 +4,14 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"runtime"
 )
 
 func main() {
+	if runtime.GOOS != "windows" {
+		fmt.Println("This command is only available for Windows.")
+		os.Exit(1)
+	}
 	cmd := exec.Command("Rundll32.exe", "user32.dll,LockWorkStation")
 	err := cmd.Run()
 	if err != nil {
