f9e8337cff07dcc95d295400e34946e524056222
yadiiiig <willemteughels@gmail.com>
2021-03-18 20:39:06 +0100

    Searchfile is now cross-platform

--- commands/searchfile.go
+++ commands/searchfile.go
@@ -8,9 +8,16 @@ import (
 )
 
 func main() {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("Something went wrong:")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 	fmt.Println(os.Args[1:][0])
 	fmt.Println("\nFound file/folder: ")
-	err := filepath.Walk("C:/Users/wille/Documents",
+	err = filepath.Walk(home,
 		func(path string, info os.FileInfo, err error) error {
 			if err != nil {
 				fmt.Println("Access Denied")
